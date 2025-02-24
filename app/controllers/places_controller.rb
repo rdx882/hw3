2425b36a0a35a84fa6deb0b0ba60e2049d454c42
Roland Deex <roland.deex@kellogg.northwestern.edu>
2025-02-24 22:46:57 +0000

    3-more html

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,19 @@
 class PlacesController < ApplicationController
   def index
+      @places = Place.all
   end
 
   def show
+    @place = Place.find_by ({"id" => params["id"]})
   end
 
   def new
+  end
+  
+  def create
+    @place = Place.new
+    @place.name = params["name"]
+    @place.save
+    redirect_to "/places"
   end
 end
